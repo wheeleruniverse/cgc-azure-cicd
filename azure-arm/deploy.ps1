1e4526cb1f267c0b4c79f17b8f7379967cf98cb7
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-03-31 08:58:08 -0400

    RSpec Packages and Tests, Azure Pipelines Changes, Increase SQLite Timeout

--- azure-arm/deploy.ps1
+++
@@ -1,9 +0,0 @@
-
-# https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-powershell
-
-New-AzResourceGroup -Name ExampleGroup -Location "Central US"
-
-New-AzResourceGroupDeployment `
-  -Name ExampleDeployment `
-  -ResourceGroupName ExampleGroup `
-  -TemplateFile c:\MyTemplates\azuredeploy.json
--- verify-app/db/seeds.rb
+++ verify-app/db/seeds.rb
@@ -1,38 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 User.delete_all
 
-users = User.create([
-  {
-    first_name: 'Justin',
-    last_name: 'Wheeler',
-    address_line1: '123 Main Street',
-    city: 'Brandon',
-    state: 'FL',
-    zip: '33511'
-  },
-  {
-    first_name: 'John',
-    last_name: 'Doe',
-    address_line1: '1 Wizard Way',
-    city: 'Tampa',
-    state: 'FL',
-    zip: '33677'
-  },
-  {
-    first_name: 'Billy',
-    last_name: 'Bob',
-    address_line1: '10 Wacky Lane',
-    city: 'Victorville',
-    state: 'CA',
-    zip: '92392'
-  }
-])
+User.create(first_name: 'Billy', last_name: 'Bob', address_line1: '10 Wacky Lane', city: 'Victorville', state: 'CA', zip: '92392')
+User.create(first_name: 'Justin', last_name: 'Wheeler', address_line1: '123 Main Street', city: 'Brandon', state: 'FL', zip: '33511')
+User.create(first_name: 'John', last_name: 'Doe', address_line1: '1 Wizard Way', city: 'Tampa', state: 'FL', zip: '33677')
 
-User.create(users)
